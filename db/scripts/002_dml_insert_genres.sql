9c2a5ec508dabe6aa4eb84359b897837ff884682
calvinhobbeson-java <calvinhobbeson.java@gmail.com>
2024-07-19 21:15:39 +0300

    Добавил SQL скрипты, создающие и заполняющие таблицы

---
+++ db/scripts/002_dml_insert_genres.sql
@@ -0,0 +1,6 @@
+insert into genres(name) values('Боевик');
+insert into genres(name) values('Фантастика');
+insert into genres(name) values('Мелодрама');
+insert into genres(name) values('Ужас');
+insert into genres(name) values('Нуар');
+insert into genres(name) values('Комедия');
---
+++ db/scripts/003_dml_insert_films.sql
@@ -0,0 +1,14 @@
+insert into films(name, description, "year", genre_id, minimal_age, duration_in_minutes, file_id)
+values('Звездные войны. Эпизод 1: Скрытая угроза', 'Приквел звездных войн, в котором есть настоящие бои на мечах.', 1999, 2, 12, 100, 1);
+insert into films(name, description, "year", genre_id, minimal_age, duration_in_minutes, file_id)
+values('Город Грехов', 'Нуарный грязный боевик, где вышибают мозги и топят в унитазах.', 2005, 5, 18, 150, 2);
+insert into films(name, description, "year", genre_id, minimal_age, duration_in_minutes, file_id)
+values('Джей и молчаливый Боб наносят ответный удар', 'Тупа лучший, если ты смотрел его в детстве.', 2001, 6, 18, 90, 3);
+insert into films(name, description, "year", genre_id, minimal_age, duration_in_minutes, file_id)
+values('Оно', 'Самый страшный фильм из существующих в природе. Привет коурлофобия.', 1990, 4, 18, 190, 4);
+insert into films(name, description, "year", genre_id, minimal_age, duration_in_minutes, file_id)
+values('Маяк', 'Один из самых странных фильмов в мире. Доказательство того, что Патиссон все таки настоящий актер', 2019, 5, 18, 150, 5);
+insert into films(name, description, "year", genre_id, minimal_age, duration_in_minutes, file_id)
+values('Ворон', 'Супер фильм, еще на сьемках погиб Брендон Ли, сын Брюса Ли, того самого', 1994, 1, 16, 100, 6);
+insert into films(name, description, "year", genre_id, minimal_age, duration_in_minutes, file_id)
+values('Дневник Бриджит Джонс', 'Мелодрама про женщину, смотреть приятно', 2004, 3, 18, 100, 7);
