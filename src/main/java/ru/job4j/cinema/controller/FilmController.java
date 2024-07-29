b08a161a4b2615577fd4d64dba910d9d113d7578
calvinhobbeson-java <calvinhobbeson.java@gmail.com>
2024-07-29 21:15:21 +0300

    Поправил представления, реализовал слои для моделей Film, File

---
+++ src/main/java/ru/job4j/cinema/controller/FilmController.java
@@ -0,0 +1,36 @@
+package ru.job4j.cinema.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.job4j.cinema.service.FilmService;
+
+@Controller
+@RequestMapping("/films")
+public class FilmController {
+
+    private final FilmService filmService;
+
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
+
+    @GetMapping
+    public String getAll(Model model) {
+        model.addAttribute("films", filmService.findAll());
+        return "films/list";
+    }
+
+    @GetMapping("/{id}")
+    public String getById(Model model, @PathVariable int id) {
+        var filmOptional = filmService.findById(id);
+        if (filmOptional.isEmpty()) {
+            model.addAttribute("message", "Фильм не найден");
+            return "errors/404";
+        }
+        model.addAttribute("film", filmOptional.get());
+        return "films/one";
+    }
+}
---
+++ src/main/java/ru/job4j/cinema/repository/FilmRepository.java
@@ -0,0 +1,13 @@
+package ru.job4j.cinema.repository;
+
+import ru.job4j.cinema.model.Film;
+
+import java.util.Optional;
+import java.util.Collection;
+
+public interface FilmRepository {
+
+    Optional<Film> findById(int id);
+
+    Collection<Film> findAll();
+}
